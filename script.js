dc2c41c0f5ac1be3788e8c997366a588f2f691b9
HEBERTE  MARTINS <114712897+BertMartins@users.noreply.github.com>
2023-04-26 13:51:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ window.onload = function() {
     const dogImages = [
       "imagem1.jpg",
       "imagem2.jpg",
-      "imagem3.jpg",
     ];
   
     // Seleciona o elemento da imagem de cachorro e define uma imagem aleatória
