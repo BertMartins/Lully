d7cf0bffa6d3bc670242b15c72b6a54768e449c8
HEBERTE  MARTINS <114712897+BertMartins@users.noreply.github.com>
2023-04-26 09:20:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ window.onload = function() {
       "imagem1.jpg",
       "imagem2.jpg",
       "imagem3.jpg",
-      // Adicione mais imagens aqui
     ];
   
     // Seleciona o elemento da imagem de cachorro e define uma imagem aleatória
     const dogImage = document.getElementById("dog-image");
     dogImage.src = dogImages[Math.floor(Math.random() * dogImages.length)];
   
-    // Seleciona o elemento do contador de dias e calcula o número de dias restantes até 1º de julho de 2023
+
     const countdown = document.getElementById("countdown");
     const today = new Date();
-    const july1st2023 = new Date("May 22, 2023");
-    const diffTime = july1st2023 - today;
+    const july1st2023 = new Date("jun 06, 2023");
+    const diffTime = June6st2023 - today;
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
     countdown.innerHTML = `Faltam ${diffDays} dias.`;
   };
